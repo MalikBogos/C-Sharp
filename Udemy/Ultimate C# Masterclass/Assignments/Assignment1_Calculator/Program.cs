3aa3b85d9b26a879c05907db4a2380e929630d04
MalikBogos <malikjandarov@gmail.com>
2024-11-22 15:31:45 +0100

    Initial commit

---
+++ Udemy/Ultimate C# Masterclass/Assignments/Assignment1_Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+var firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+var secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("What do you want to do with these numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+var userInput = Console.ReadLine().ToUpper();
+
+//if(userInput == "A")
+//{
+//    Addition(firstNumber, secondNumber);
+//} else if(userInput == "S")
+//{
+//    Subtraction(firstNumber, secondNumber);
+//} else if(userInput == "M")
+//{
+//    Multiplication(firstNumber, secondNumber);
+//} else
+//{
+//    Console.WriteLine("Invalid option");
+//}
+//Console.WriteLine("Press any key to close");
+//Console.ReadKey();
+
+switch (userInput)
+{
+    case "A":
+        Addition(firstNumber, secondNumber);
+        break;
+
+    case "S":
+        Subtraction(firstNumber, secondNumber);
+        break;
+
+    case "M":
+        Multiplication(firstNumber, secondNumber);
+        break;
+
+    default:
+        Console.WriteLine("Invalid option");
+        break;
+}
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
+
+
+static void Addition(int firstNumber, int secondNumber)
+{
+    var sum = firstNumber + secondNumber;
+    Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+}
+
+static void Subtraction(int firstNumber, int secondNumber)
+{
+    var difference = firstNumber - secondNumber;
+    Console.WriteLine($"{firstNumber} - {secondNumber} = {difference}");
+}
+
+static void Multiplication(int firstNumber, int secondNumber)
+{
+    var product = firstNumber * secondNumber;
+    Console.WriteLine($"{firstNumber} * {secondNumber} = {product}");
+}
---
+++ Udemy/Ultimate C# Masterclass/Assignments/Assignment2_ToDoList/Program.cs
@@ -0,0 +1,96 @@
+﻿printKeuzeMenu();
+string userInput = Console.ReadLine().ToUpper();
+List<string> TODOlist = new List<string>();
+
+while (userInput != "E")
+{
+    switch (userInput)
+    {
+        case "S":
+            if (TODOlist.Count == 0)
+            {
+                Console.WriteLine("No TODOs have been added yet.");
+                break;
+            }
+            else
+            {
+                printTODOlist();
+                break;
+            }
+
+        case "A":
+            do
+            {
+                Console.WriteLine("Enter a TODO description: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == string.Empty)
+                {
+                    Console.WriteLine("The description cannot be empty.");
+                }
+                else if (TODOlist.Contains(userInput))
+                {
+                    Console.WriteLine("The description must be unique.");
+                }
+                else
+                {
+                    TODOlist.Add(userInput);
+                    Console.WriteLine("TODO succesfully added: " + userInput);
+                    break;
+                }
+            } while (true);
+            break;
+
+        case "R":
+            Console.WriteLine("Select the index of the TODO you want to remove");
+            printTODOlist();
+            bool isValid = int.TryParse(Console.ReadLine(), out int TODOremover);
+
+            if (isValid)
+            {
+                Console.WriteLine("Valid integer: " + TODOremover);
+                string tijdelijkeWaarde = TODOlist[TODOremover - 1];
+                TODOlist.Remove(TODOlist[TODOremover - 1]);
+                Console.WriteLine("TODO removed: " + tijdelijkeWaarde);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            break;
+
+
+        default:
+            Console.WriteLine("default");
+            break;
+    }
+    printKeuzeMenu();
+    userInput = Console.ReadLine().ToUpper();
+}
+
+void printKeuzeMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+}
+
+void printTODOlist()
+{
+    if (TODOlist.Count == 0)
+    {
+        Console.WriteLine("No TODOs have been added yet.");
+    }
+    else
+    {
+        for (int i = 0; i < TODOlist.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {TODOlist[i]}");
+        }
+    }
+
+}
