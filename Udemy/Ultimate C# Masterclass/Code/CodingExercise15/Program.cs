267b2423d7607def0c34d8fb8fe2b60a837f4a62
MalikBogos <malikjandarov@gmail.com>
2024-11-29 18:55:21 +0100

    Added new lessons & Coding exercise

---
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise15/Program.cs
@@ -0,0 +1,20 @@
+﻿HotelBooking test = new HotelBooking("malik", new DateTime(20,06,03), 5);
+Console.WriteLine(test.GuestName);
+Console.WriteLine(test.StartDate);
+Console.WriteLine(test.EndDate);
+
+Console.ReadKey();
+
+public class HotelBooking
+{
+    public string GuestName;
+    public DateTime StartDate;
+    public DateTime EndDate;
+
+    public HotelBooking(string guestName, DateTime startDate, int lengthOfStayInDays)
+    {
+        GuestName = guestName;
+        StartDate = startDate;
+        EndDate = startDate.AddDays(lengthOfStayInDays);
+    }
+}
---
+++ Udemy/Ultimate C# Masterclass/Code/Les67/Program.cs
@@ -0,0 +1,12 @@
+﻿var rectangle1 = new Rectangle();
+
+Console.WriteLine("Width is: " + rectangle1.width);
+Console.WriteLine("Height is: " + rectangle1.heigth);
+
+Console.ReadKey();  
+
+class Rectangle
+{
+    public int width;
+    public int heigth;
+}
