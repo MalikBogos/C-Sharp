0619ff7e7292fa599c402c69e411079792ebc434
MalikBogos <malikjandarov@gmail.com>
2024-12-02 17:46:37 +0100

    Added coding exercise16

---
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise16/Program.cs
@@ -0,0 +1,27 @@
+﻿Triangle uitprobering = new Triangle(5, 10);
+
+Console.WriteLine(uitprobering.AsString());
+Console.WriteLine(uitprobering.CalculateArea());
+Console.ReadKey();
+
+public class Triangle
+{
+    private int _base;
+    private int _height;
+
+    public Triangle(int @base, int height)
+    {
+        _base = @base;
+        _height = height;
+    }
+    public int CalculateArea()
+    {
+        int area = ((_base * _height) / 2);
+        return area;
+    }
+
+    public string AsString ()
+    {
+        return $"Base is {_base}, height is {_height}";
+    }
+}
