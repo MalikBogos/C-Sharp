aa6a7db16c9b8b4d94c1d0948c04499e1352423e
MalikBogos <malikjandarov@gmail.com>
2024-12-02 17:27:44 +0100

    Added Les71 & Les 72 (adding methods & encapsulation)

---
+++ Udemy/Ultimate C# Masterclass/Code/Les72/Program.cs
@@ -0,0 +1,41 @@
+﻿var rectangle1 = new Rectangle(5, 10);
+var calculator = new ShapesMeasurementsCalculator();
+
+Console.WriteLine("Width is: " + rectangle1.Width);
+Console.WriteLine("Height is: " + rectangle1.Height);
+Console.WriteLine("Circumference is: " + calculator.CalculateRectangleCircumference(rectangle1));
+Console.WriteLine("Area is: " + calculator.CalculateRectangleCircumference(rectangle1));
+
+var rectangle2 = new Rectangle(7, 9);
+
+Console.WriteLine("Width is: " + rectangle2.Width);
+Console.WriteLine("Height is: " + rectangle2.Height);
+Console.WriteLine("Circumference is: " + calculator.CalculateRectangleCircumference(rectangle2));
+Console.WriteLine("Area is: " + calculator.CalculateRectangleCircumference(rectangle2));
+
+Console.ReadKey();
+
+class Rectangle
+{
+    public int Width;
+    public int Height;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
+
+class ShapesMeasurementsCalculator
+{
+    public int CalculateRectangleCircumference(Rectangle rectangle)
+    {
+        return 2 * rectangle.Width + 2 * rectangle.Height;
+    }
+
+    public int CalculateRectangleArea(Rectangle rectangle)
+    {
+        return rectangle.Width * rectangle.Height;
+    }
+}
