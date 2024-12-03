3656dea9582a126a986f889e0f1e68a9687f71d3
MalikBogos <malikjandarov@gmail.com>
2024-12-03 16:13:09 +0100

    Started CodingExercise, added Les77

---
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise17/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Dog
+{
+    private string _name;
+    private string _breed;
+    private int _weight; 
+
+    public Dog (string name, string breed, int weight) 
+    {
+
+    }
+
+    public Dog (string name, int weight, string breed = "mixed-breed")
+    {
+
+    }
+}
---
+++ Udemy/Ultimate C# Masterclass/Code/Les77/Program.cs
@@ -0,0 +1,67 @@
+﻿var medicalAppointment = new MedicalAppointment("Malik", new DateTime(2024, 5, 4));
+
+//medicalAppointment.Reschedule(new DateTime(2023, 4, 4));
+
+var medicalAppointmentPrinter = new MedicalAppointmentPrinter();
+var appointmentUnknownPatient = new MedicalAppointment();
+
+medicalAppointmentPrinter.Print(medicalAppointment);
+
+Console.ReadKey();
+
+class MedicalAppointmentPrinter
+{
+    public void Print(MedicalAppointment medicalAppointment)
+    {
+        Console.WriteLine("Appointment will take place on " + medicalAppointment.GetDate());
+    }
+}
+
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+    public DateTime GetDate() => _date;
+
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    //public MedicalAppointment(string patientName) :
+    //    this(patientName, 7) // constructor chaining
+    //{
+    //}
+
+    //public MedicalAppointment(string patientName) // in case of ambiguity, method without optional parameter will be used
+    //{
+    //    _patientName = patientName;
+    //}
+
+    public MedicalAppointment(string patientName = "Unknown", int daysFromNow = 7) // daysFromNow is an optional parameter
+    {
+        _patientName = patientName;
+        _date = DateTime.Now.AddDays(daysFromNow);
+    }
+
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+        var printer = new MedicalAppointmentPrinter();
+        printer.Print(this);
+    }
+
+    public void OverwriteMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day); // method overloading
+    }
+
+    public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd) //doesn't work in all cases for example, December + 5 months
+    {
+        _date = new DateTime(_date.Year,
+            _date.Month + monthsToAdd,
+            _date.Day + daysToAdd);
+    }
+}
