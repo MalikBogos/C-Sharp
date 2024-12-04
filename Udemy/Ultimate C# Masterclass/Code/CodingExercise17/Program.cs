566b32c00fd21e0205edfdfecd03d3e9996bf318
MalikBogos <malikjandarov@gmail.com>
2024-12-04 15:57:58 +0100

    Added coding exercise 17

--- Udemy/Ultimate C# Masterclass/Code/CodingExercise17/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise17/Program.cs
@@ -1,4 +1,12 @@
-﻿public class Dog
+﻿Dog ding = new Dog("Lucky", "german shepherd", 40);
+Dog hond = new Dog("Tina", "shar pei", 25);
+
+
+Console.WriteLine(hond.Describe());
+Console.WriteLine(ding.Describe());
+Console.ReadKey();
+
+public class Dog
 {
     private string _name;
     private string _breed;
@@ -6,11 +14,36 @@
 
     public Dog (string name, string breed, int weight) 
     {
-
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+        Describe();
     }
 
     public Dog (string name, int weight, string breed = "mixed-breed")
     {
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+        Describe();
+    }
 
+    public string Describe()
+    {
+        string size;
+        if (_weight < 5)
+        {
+            size = "tiny";
+        }
+        else if (_weight >= 5 || _weight < 30)
+        {
+            size = "medium";
+        }
+        else
+        {
+            size = "large";
+        }
+
+        return $"This dog is named {_name}, it's a {_breed} and it weighs {_weight} kilograms, so it's a {size} dog.";
     }
 }
