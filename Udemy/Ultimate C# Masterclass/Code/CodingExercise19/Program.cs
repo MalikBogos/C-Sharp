a7d20a16eae68aabe9f090d8e0ad18542dde800b
MalikBogos <malikjandarov@gmail.com>
2024-12-06 23:57:56 +0100

    Started Coding Exercise 19

---
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise19/Program.cs
@@ -0,0 +1,11 @@
+﻿public class DailyAccountState
+{
+    public int InitialState { get; }
+    public int SumOfOperations { get; }
+    public DailyAccountState (int initialState, int sumOfOperations)
+    {
+        InitialState = initialState;
+        SumOfOperations = sumOfOperations;
+    }
+
+}
