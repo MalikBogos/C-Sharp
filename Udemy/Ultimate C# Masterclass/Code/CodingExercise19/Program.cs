fccca1e84008129845373857cee553f999d70936
MalikBogos <malikjandarov@gmail.com>
2024-12-09 18:24:08 +0100

    Added CodingExercise19

--- Udemy/Ultimate C# Masterclass/Code/CodingExercise19/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise19/Program.cs
@@ -1,11 +1,30 @@
-﻿public class DailyAccountState
+﻿DailyAccountState uitprobering = new DailyAccountState(2000, +2900);
+
+Console.WriteLine(uitprobering.Report);
+Console.ReadKey();
+public class DailyAccountState
 {
     public int InitialState { get; }
     public int SumOfOperations { get; }
+    
     public DailyAccountState (int initialState, int sumOfOperations)
     {
         InitialState = initialState;
         SumOfOperations = sumOfOperations;
+    }
+
+    private int _EndOfDayState;
+
+    public int EndOfDayState
+    {
+        get { return InitialState + SumOfOperations; }
+    }
+
+    private int _report;
+
+    public string Report
+    {
+        get => $"Day: {DateTime.Now.Day}, month: {DateTime.Now.Month}, year: {DateTime.Now.Year}, initial state: {InitialState}, end of day state: {EndOfDayState}";
     }
 
 }
