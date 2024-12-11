e3ca9550eaa35263332d79e810a2830fd83fd141
MalikBogos <malikjandarov@gmail.com>
2024-12-11 23:36:09 +0100

    Started Coding Exercise 21

---
+++ Udemy/Ultimate C# Masterclass/Code/CodingExercise21/Program.cs
@@ -0,0 +1,11 @@
+﻿public static class StringsTransformator
+{
+    public static string TransformSeperators
+    (
+        string input,
+        string originalSeperator, 
+        string targetSeperator)
+    {
+        return "for now";
+    }
+}
