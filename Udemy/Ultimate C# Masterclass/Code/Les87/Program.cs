71faf4fde6e47c5cd41744926f882071cb8fce84
MalikBogos <malikjandarov@gmail.com>
2024-12-24 22:15:46 +0100

    Les86 refactoring

---
+++ Udemy/Ultimate C# Masterclass/Code/Les87/Program.cs
@@ -0,0 +1,72 @@
+﻿var names = new Names();
+var path = names.BuildFilePath();
+if (File.Exists(path))
+{
+    Console.WriteLine("Names file already exists. Loading names.");
+    names.ReadFromTextFile();
+}
+else
+{
+    Console.WriteLine("Names file does not yet exist");
+
+    //let's imagine they are given by the user
+    names.AddName("John");
+    names.AddName("not a valid name");
+    names.AddName("Claire");
+    names.AddName("123 definitely not a valid name");
+
+    Console.WriteLine("Saving names to a file");
+    names.WriteToTextFile();
+}
+Console.WriteLine(names.Format);
+Console.ReadKey();
+
+class NamesValidator
+{
+    public bool IsValid(string name)
+    {
+        return name.Length >= 2 && name.Length < 25 && char.IsUpper(name[0]) && name.All(char.IsLetter);
+    }
+}
+class Names
+{
+    private List<string> _names = new List<string>();
+    private readonly NamesValidator _namesValidator = new NamesValidator();
+
+    public void AddName(string name)
+    {
+        if (new NamesValidator().IsValid(name))
+        {
+            _names.Add(name);
+        }
+    }
+
+    
+
+    public void ReadFromTextFile()
+    {
+        var fileContents = File.ReadAllText(BuildFilePath());
+        var namesFromFile = fileContents.Split(Environment.NewLine).ToList();
+        foreach (var name in namesFromFile)
+        {
+            AddName(name);
+        }
+    }
+
+    public void WriteToTextFile()
+    {
+        File.WriteAllText(BuildFilePath(), Format());
+    }
+
+    public string BuildFilePath()
+    {
+        // we could imagine this is much more complicated,
+        // for example provided by the user and validated
+        return "names.txt";
+    }
+
+    public string Format()
+    {
+        return string.Join(Environment.NewLine, _names);
+    }
+}
