4bdc6330386a2fdebe5690534f9399f142e5b59c
MalikBogos <malikjandarov@gmail.com>
2025-01-06 17:23:37 +0100

    Added seperate class files for Names, NamesValidator, StringsTextualRepository

---
+++ Udemy/Ultimate C# Masterclass/Code/Les90/Names.cs
@@ -0,0 +1,23 @@
+﻿class Names
+{
+    public List<string> All { get; } = new List<string>();
+    private readonly NamesValidator _namesValidator = new NamesValidator();
+
+    public void AddNames(List<string> stringsFromFile)
+    {
+        foreach (var name in stringsFromFile)
+        {
+            AddName(name);
+        }
+    }
+
+
+
+    public void AddName(string name)
+    {
+        if (new NamesValidator().IsValid(name))
+        {
+            All.Add(name);
+        }
+    }
+}
---
+++ Udemy/Ultimate C# Masterclass/Code/Les90/Program.cs
@@ -0,0 +1,42 @@
+﻿var names = new Names();
+var path = new NamesFilePathBuilder().BuildFilePath();
+var stringsTextualRepository = new StringsTextualRepository();
+if (File.Exists(path))
+{
+    Console.WriteLine("Names file already exists. Loading names.");
+    var stringsFromFile = stringsTextualRepository.Read(path);
+    names.AddNames(stringsFromFile);
+}
+else
+{
+    Console.WriteLine("Names file does not yet exist");
+
+    //let's imagine they are given by the user
+    names.AddName("John");
+    names.AddName("not a valid name");
+    names.AddName("Claire");
+    names.AddName("123 definitely not a valid name");
+
+    Console.WriteLine("Saving names to a file");
+    stringsTextualRepository.Write(path, names.All);
+}
+Console.WriteLine(new NamesFormatter().Format(names.All));
+Console.ReadKey();
+
+class NamesFilePathBuilder
+{
+    public string BuildFilePath()
+    {
+        // we could imagine this is much more complicated,
+        // for example provided by the user and validated
+        return "names.txt";
+    }
+}
+
+class NamesFormatter
+{
+    public string Format(List<string> names)
+    {
+        return string.Join(Environment.NewLine, names);
+    }
+}
