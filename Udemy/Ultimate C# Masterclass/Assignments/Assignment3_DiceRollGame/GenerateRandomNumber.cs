225b2508d072e1501d45228f292fc34367dd2fe9
MalikBogos <malikjandarov@gmail.com>
2025-01-10 16:02:08 +0100

    Added Validation method with 2 parameters to the Check class

--- Udemy/Ultimate C# Masterclass/Assignments/Assignment3_DiceRollGame/GenerateRandomNumber.cs
+++ Udemy/Ultimate C# Masterclass/Assignments/Assignment3_DiceRollGame/GenerateRandomNumber.cs
@@ -4,7 +4,6 @@
 
     public void DisplayNumber()
     {
-
         Console.WriteLine(Number);
     }
 }
--- Udemy/Ultimate C# Masterclass/Assignments/Assignment3_DiceRollGame/Program.cs
+++ Udemy/Ultimate C# Masterclass/Assignments/Assignment3_DiceRollGame/Program.cs
@@ -1,29 +1,52 @@
 ﻿//RandomNumberGenerator number = new RandomNumberGenerator();
 
 GenerateRandomNumber number = new GenerateRandomNumber();
+int randomNumber = number.Number;
 number.DisplayNumber();
 GenerateMessage.RolledDie();
 
 Input input = new Input();
-input.ReadInput();
+int userInput = input.ReadInput();
+
+Check check = new Check();
+check.Validation(userInput, randomNumber);
+
 
 Console.ReadKey();
 
-class Input
+public class Input
 {
-    public void ReadInput()
+    public int ReadInput()
     {
-        int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        if(int.TryParse(input, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Defaulting to 0.");
+            return 0;
+        }
+        
     }  
 }
 
 public class Check
 {
-    void userInput(Input input)
+    public void Validation(int input, int randomNumber)
     {
-        if (input < 0)
+        if (input == 0)
         {
-            GenerateMessage.IncorrectInput();
+            Console.WriteLine("Input is zero.");
+        } 
+        else if (input == randomNumber)
+        {
+            GenerateMessage.YouWin();
+        }
+        else
+        {
+            Console.WriteLine($"Input is: {input}");
         }
     }
 }
