ad093da5f352099c1102c42cfddf01a5334f9617
MalikBogos <malikjandarov@gmail.com>
2025-01-20 23:58:58 +0100

    Updated Les96

--- Udemy/Ultimate C# Masterclass/Code/Les96/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/Les96/Program.cs
@@ -5,22 +5,45 @@ Console.ReadKey();
 public class GuessingGame
 {
     private readonly Dice _dice;
+    private readonly ConsoleReader _consoleReader;
     private const int InitialTries = 3;
     public GuessingGame(Dice dice)
     {
         _dice = dice;
     }
 
-    public void Play()
+    public bool Play()
     {
         var diceRollResult = _dice.Roll();
         Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries.");
 
         var triesLeft = InitialTries;
         while (triesLeft > 0) {
-            var guess = ReadInteger("Enter a number");
-            --triesLeft
+            var guess = ConsoleReader.ReadInteger("Enter a number");
+            if(guess == diceRollResult)
+            {
+                return true;
+            }
+            Console.WriteLine("Wrong number.");
+            --triesLeft;
                 }
+        return false;
+    }
+
+    
+}
+
+public static class ConsoleReader
+{
+    public static int ReadInteger(string message)
+    {
+        int result;
+        do
+        {
+            Console.WriteLine(message);
+        }
+        while (!int.TryParse(Console.ReadLine(), out result));
+        return result;
     }
 }
 
