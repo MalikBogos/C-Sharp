e91233b0e475976f478ad8d0e9f6926c9ac36d46
MalikBogos <malikjandarov@gmail.com>
2025-03-03 23:59:07 +0100

    Started Les100

---
+++ Udemy/Ultimate C# Masterclass/Code/Les100/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.ReadKey();
+
+public class Cheddar
+{
+    public string Name => "Cheddar cheese";
+    public int AgedForMonths { get; }
+}
+
+public class TomatoSauce
+{
+    public string Name => "Tomato sauce";
+    public int TomatosIn100Grams { get; }
+}
+
+public class Mozarella
+{
+    public string Name => "Mozarella";
+    public bool IsLight { get; }
+}
