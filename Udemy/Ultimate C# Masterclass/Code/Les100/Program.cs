be09dc2e38a4e29eda00b7b196a629e345aa0d72
MalikBogos <malikjandarov@gmail.com>
2025-03-04 23:56:29 +0100

    Finished Les100

--- Udemy/Ultimate C# Masterclass/Code/Les100/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/Les100/Program.cs
@@ -1,5 +1,16 @@
 ﻿Console.ReadKey();
 
+public class Pizza // problem to be solved with polymorphism
+{
+    private List<Ingredient> _ingredients = new List<Ingredient>();
+
+    public void AddIngredient(Ingredient ingredient) =>
+        _ingredients.Add(ingredient);
+
+    public string Describe() =>
+        $"This is a pizza with {String.Join(", ", _ingredients)}";
+}
+
 public class Cheddar
 {
     public string Name => "Cheddar cheese";
