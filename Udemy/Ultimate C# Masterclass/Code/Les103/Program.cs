87e25e43be98f7077c7932e047b3fdd395ef6d93
MalikBogos <malikjandarov@gmail.com>
2025-03-08 20:20:47 +0100

    Finished Les103

--- Udemy/Ultimate C# Masterclass/Code/Les103/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/Les103/Program.cs
@@ -5,8 +5,19 @@
 
 //Console.WriteLine(pizza.Describe());
 
+
+var ingredient = new Ingredient();
+ingredient.PublicField = 10;
+
 var cheddar = new Cheddar();
-Console.WriteLine(cheddar.PublicMethod());
+cheddar.PublicField = 20;
+
+Console.WriteLine("Value in ingredient: " + ingredient.PublicField);
+Console.WriteLine("Value in cheddar: " + cheddar.PublicField);
+
+//var cheddar = new Cheddar();
+//Console.WriteLine(cheddar.PublicMethod());
+//Console.WriteLine(cheddar.ProtectedMethod());
 //Console.WriteLine(cheddar.PrivateMethod());
 
 Console.ReadKey();
@@ -24,8 +35,13 @@ public class Pizza // problem to be solved with polymorphism
 
 public class Ingredient
 {
+    public int PublicField;
+
     public string PublicMethod() =>
         "This method is PUBLIC in the Ingredient class.";
+
+    protected string ProtectedMethod() =>
+        "This method is PROTECTED in the Ingredient class";
 
     private string PrivateMethod() =>
         "This method is PRIVATE in the Ingredient class";
@@ -38,6 +54,7 @@ public class Cheddar : Ingredient
     public void UseMethodsFromBaseClass()
     {
         Console.WriteLine(PublicMethod());
+        Console.WriteLine(ProtectedMethod());
         //Console.WriteLine(PrivateMethod());
     }
 }
