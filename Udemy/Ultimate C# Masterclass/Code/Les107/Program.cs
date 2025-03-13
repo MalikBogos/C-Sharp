e212a74f214106c42c46fdde120959fee66345c6
MalikBogos <malikjandarov@gmail.com>
2025-03-13 23:55:19 +0100

    Continued work on Les107 by NumbersSumCalculator class and PositiveNumbersSumCalculator classes

--- Udemy/Ultimate C# Masterclass/Code/Les107/Program.cs
+++ Udemy/Ultimate C# Masterclass/Code/Les107/Program.cs
@@ -1,2 +1,50 @@
 ﻿var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, 19 };
-var sum = new NumbersSumCalculator().Calculate(numbers);
+bool shallAddPositiveOnly = true;
+
+int sum;
+
+if (shallAddPositiveOnly)
+{
+    sum = new PositiveNumbersSumCalculator().Calculate(numbers);
+}
+else
+{
+    sum = new NumbersSumCalculator().Calculate(numbers);
+}
+
+Console.WriteLine("Sum is: " + sum);
+
+
+
+Console.ReadKey();
+
+public class NumbersSumCalculator
+{
+    public int Calculate(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+
+    }
+}
+
+public class PositiveNumbersSumCalculator
+{
+    public int Calculate(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            if(number > 0)
+            {
+                sum += number;
+            }
+        }
+        return sum;
+
+    }
+}
